a0b9cd6aeee6917e194b9d794e1c4caab412890c
Kantee Charoensedtasin <kantee2540@hotmail.com>
2021-03-02 00:59:40 +0700

    config data to config.json

--- config/config.py
+++ config/config.py
@@ -1,11 +1,15 @@
 import pymysql
+import json
+
+with open("config.json") as configs:
+    config = json.load(configs)
 
 
 def connect_db():
-    connection = pymysql.connect(host="localhost",
-                                 user="root",
-                                 password="",
-                                 db="Dharma",
+    connection = pymysql.connect(host=config["host_database"],
+                                 user=config["user"],
+                                 password=config["password"],
+                                 db=config["database"],
                                  charset="utf8mb4",
                                  cursorclass=pymysql.cursors.DictCursor,
                                  autocommit=True)
